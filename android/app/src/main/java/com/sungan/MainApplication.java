8a993290c6d60723913283938af6c048bb139f72
junggernaut <seungan1419@gmail.com>
2022-06-08 18:41:52 +0900

    fix: project id

--- android/app/src/main/java/com/sungan/MainApplication.java
+++ android/app/src/main/java/com/sungan/MainApplication.java
@@ -76,7 +76,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.sungan.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.BetterWorld.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
