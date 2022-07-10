5797e331ea193e26d211a6a1d5252d25b6978854
Sehan MacPro <ianparkfirst@gmail.com>
2022-07-10 15:51:09 +0900

    feat: ios icon update

--- src/components/common/ListEmptyComponent.tsx
+++ src/components/common/ListEmptyComponent.tsx
@@ -8,7 +8,7 @@ import {Span} from './Span';
 export default function ListEmptyComponent({h}) {
   return (
     <Div h={h} itemsCenter justifyCenter bgWhite>
-      <Img rounded10 h100 w100 source={IMAGES.betterWorldPlanet}></Img>
+      <Img rounded10 h80 w80 source={IMAGES.betterWorldPlanet}></Img>
       <Div justifyCenter itemsCenter mt16>
         <Span bold>내용이 없습니다.</Span>
       </Div>
