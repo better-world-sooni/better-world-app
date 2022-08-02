51cd91248d076078c302e89a6c26d75bcc1e6dd8
junggernaut <seungan1419@gmail.com>
2022-08-03 00:00:51 +0900

    feat: android deploy 1st

--- android/app/src/main/java/com/betterworld/MainApplication.java
+++ android/app/src/main/java/com/betterworld/MainApplication.java
@@ -18,34 +18,34 @@ import com.brentvatne.react.ReactVideoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for
-          // example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
 
-        @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      // 3. Instantiate an instance of the CodePush runtime and add it to the list of
+      // existing packages, specifying the right deployment key. If you don't already
+      // have it, you can run "appcenter codepush deployment list -a
+      // <ownerName>/<appName> -k" to retrieve your key.
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new CodePush("mLiDU9OEAUxUwBrByrLZGN-rtUpRzGUdTswyE", MainApplication.this, BuildConfig.DEBUG));
+    }
 
-      };
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -60,7 +60,8 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * Loads Flipper in React Native templates. Call this in the onCreate method
+   * with something like
    * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
@@ -71,9 +72,9 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
+         * We use reflection here to pick up the class that initializes Flipper,
+         * since Flipper library is not available in release mode
+         */
         Class<?> aClass = Class.forName("com.BetterWorld.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
